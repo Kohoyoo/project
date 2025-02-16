d6f7b09502932f06f89b436fe255872bcdc99e0f
Kohoyoo <samuelwu2008@gmail.com>
2025-02-15 22:47:52 -0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Blurb"> 
+              Hi, we’re NjRescue, and we aim to partner with local New Jersey animal shelters and connect a large network of rescue animals to make adopting a pet easier. If you can’t adopt, no worries! You can donate to help any one of our animals and receive monthly updates on them. We hope to continue to better New Jersey’s adoption rates through our system, and every dog saved is a step towards a brighter future.
+        </div>
+      <img className="1Dog">
+
+      </img>  
+        
+      <img className="2Dog">
+      </img>
+    </div>
+  );
+}
+
+export default App;
